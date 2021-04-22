437043626d759f6744bc9c543d8cb4de1032ae53
Biancabenck <78942369+Biancabenck@users.noreply.github.com>
2021-04-21 21:48:26 -0400

    update

--- src/test/java/BIanca/JUnitIntroTest.java
+++ src/test/java/BIanca/JUnitIntroTest.java
@@ -40,6 +40,7 @@ public class JUnitIntroTest {
 		System.out.println("---------");
 		System.out.println("After Class");
 		System.out.println("---------");
+		System.out.println("---------");
 	}
 	
 
